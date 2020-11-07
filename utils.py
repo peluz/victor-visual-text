6070b84a970c66ae13023c90b9c9dad2e2279fa3
peluz <pedrohluzaraujo@gmail.com>
2020-11-07 19:41:13 +0000

    train text ulmfit

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from fastai.text.all import *
 from fastai.vision.all import *
 from sklearn.metrics import classification_report
+import torch
 
 
 def get_dls(path, bs, size):
@@ -17,6 +18,13 @@ def get_dls(path, bs, size):
     return dblock.dataloaders(path, bs=bs)
 
 def evaluate(learn, dls, idx=1):
-    preds, targets = learn.get_preds(idx)
+    preds, targets = learn.get_preds(dl=dls, ds_idx=idx)
     preds = np.argmax(preds, axis=1)
-    print(classification_report(targets, preds, target_names=dls.vocab, digits=4))
+    print(classification_report(targets, preds, target_names=dls.vocab[-1], digits=4))
+    
+def evaluate_ensemble(fwd, bwd, fwd_dl, bwd_dl, idx=1):
+    preds_fwd, targets = fwd.get_preds(dl=fwd_dl, ds_idx=idx)
+    preds_bwd, _ = bwd.get_preds(dl=bwd_dl, ds_idx=idx)
+    preds = (preds_fwd + preds_bwd)/2
+    preds = np.argmax(preds, axis=1)
+    print(classification_report(targets, preds, target_names=fwd.dl.vocab[-1], digits=4))
