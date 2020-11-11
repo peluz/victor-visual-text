1ce6042249afb465a2f11671639381eb1ff0277f
peluz <pedrohluzaraujo@gmail.com>
2020-11-11 21:49:06 +0000

    evaluate fusion

--- utils.py
+++ utils.py
@@ -17,14 +17,14 @@ def get_dls(path, bs, size):
                                Normalize.from_stats(*imagenet_stats)])
     return dblock.dataloaders(path, bs=bs)
 
-def evaluate(learn, dls, idx=1):
-    preds, targets = learn.get_preds(dl=dls, ds_idx=idx)
+def evaluate(learn, dls):
+    preds, targets = learn.get_preds(dl=dls)
     preds = np.argmax(preds, axis=1)
     print(classification_report(targets, preds, target_names=dls.vocab[-1], digits=4))
     
-def evaluate_ensemble(fwd, bwd, fwd_dl, bwd_dl, idx=1):
-    preds_fwd, targets = fwd.get_preds(dl=fwd_dl, ds_idx=idx)
-    preds_bwd, _ = bwd.get_preds(dl=bwd_dl, ds_idx=idx)
+def evaluate_ensemble(fwd, bwd, fwd_dl, bwd_dl):
+    preds_fwd, targets = fwd.get_preds(dl=fwd_dl)
+    preds_bwd, _ = bwd.get_preds(dl=bwd_dl)
     preds = (preds_fwd + preds_bwd)/2
     preds = np.argmax(preds, axis=1)
     print(classification_report(targets, preds, target_names=fwd.dl.vocab[-1], digits=4))
